96e181931bbca0769d889ed95042e5b54d431ade
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2016-08-17 18:56:37 +0900

    added ros opencv sample

---
+++ ros/opencv_exercise/build/ros_start4/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "ros_start4"
+PROJECT_SPACE_DIR = "/home/jsupratman13/github/workspace/ros/opencv_exercise/devel"
+PROJECT_VERSION = "0.0.0"

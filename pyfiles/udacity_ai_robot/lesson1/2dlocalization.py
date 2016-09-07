d434215846624b9106be55bf207c423a905e4e6b
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2016-09-07 17:54:36 +0900

    added 2d localization example

--- pyfiles/udacity_ai_robot/lesson1/2dlocalization.py
+++ pyfiles/udacity_ai_robot/lesson1/2dlocalization.py
@@ -16,11 +16,6 @@ colors = [['R', 'G', 'G', 'R', 'R'],
 measurements = ['G', 'G', 'G', 'G', 'G']
 
 motions = [[0,0],[0,1],[1,0],[1,0],[0,1]]
-sr = 0.7
-sw = 1-sr
-
-pm = 0.8
-ps = 1-pm
 
 def UniformDist(world):
 	q = []
@@ -30,29 +25,7 @@ def UniformDist(world):
 		for j in range(len(world[0])):
 			r.append(1./n)
 		q.append(r)
-	show(q)
 	return q
-
-def sense(p, colors, measurement):
-	aux = [[0.0 for row in range(len(p[0]))] for col in range(len(p))]
-	s=0.0
-	for i in range(len(p)):
-		for j in range(len(p[i])):
-			hit = (measurement == colors[i][j])
-			aux[i][j] = p[i][j] * (hit * sr + (1-hit) * sw)
-			s += aux[i][j]
-	for i in range(len(aux)):
-		for j in range(len(p[i])):
-			aux[i][j] /= s
-	return aux
-
-def move(p, motion):
-	aux = [[0.0 for row in range(len(p[0]))] for col in range(len(p))]
-	for i in range(len(p)):
-		for j in range(len(p[i])):
-			aux[i][j] = (pm*p[(i-motion[0])%len(p)][(j-motion[1])%len(p[i])]) + (ps * p[i][j])
-	return aux
-
 #sense
 def BayesRule(p,measurement, colors, sensor_right):
 	q=[]
@@ -94,17 +67,9 @@ def TotalProb(p,U,pExact):
 
 def localize(colors, measurements, motions,sensor_right=1.0,p_move=1.0):
 	p = UniformDist(colors)
-	pinit = 1.0 / float(len(colors))/float(len(colors[0]))
-	q = [[pinit for row in range(len(colors[0]))] for col in range(len(colors))]
 	for i in range(len(motions)):
 		p = TotalProb(p, motions[i],p_move)
 		p = BayesRule(p, measurements[i], colors, sensor_right)
-		q = move(q, motions[i])
-		q = sense(q, colors, measurements[i])
-		show(p)
-		print 'ans'
-		show(q)
-		print ' '
 	return p
 
 def show(p):
